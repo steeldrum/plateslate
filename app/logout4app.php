250a4dd87530b67028689403e7f77b3df5711fe8
steeldrum <tsoucy@me.com>
2013-11-13 10:27:40 -0500

    plateslate app changes migration to pg and new observer menu report support

--- app/logout4app.php
+++ app/logout4app.php
@@ -4,10 +4,14 @@ $Revision:: 175                        $: Revision of last commit
 $LastChangedBy::                       $: Author of last commit
 $LastChangedDate:: 2012-01-03 14:44:48#$: Date of last commit
 ***************************************/
-$errorMessage = 'false';
+// tjs 131113
+//$errorMessage = 'false';
 //require_once( "common.inc.php" );
-//require_once( "../plateslate/config.php" );
+require_once( "./../config.php" );
+$observer_url = OBSERVER_URL;
+//echo "observer_url $observer_url";
 session_start();
 $_SESSION["member"] = "";
-echo $errorMessage;
+//echo $errorMessage;
+echo $observer_url;
 ?>
